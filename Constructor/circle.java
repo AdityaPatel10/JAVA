aaa1f82e47e3c4a57843ad74a654bdcde3b60d4c
Aditya Patel <aditya97556@gmail.com>
2023-08-19 18:46:26 +0530

    Updating Area Of Circle

--- Constructor/circle.java
+++ Constructor/circle.java
@@ -1,26 +1,29 @@
 class circle {
     double area, rad;
+    circle(){
+        rad=1;
+    }
     circle(int r) {
         rad = r;
     }
     circle(double r) {
         rad = r;
     }
-    void Area() {
+    void Calculate_area() {
         area = 3.14 * rad * rad;
     }
     void display(){
-        System.out.println("Area of circle at radius "+ rad + " is= " + area);
+        System.out.println(String.format("Area of circle at radius " + rad + " is= "+"%.2f",area));
     }
     public static void main(String[] args) {
         circle c1 = new circle(5);
-        c1.Area();
+        c1.Calculate_area();
         c1.display();
         circle c2 = new circle(6.8);
-        c2.Area();
+        c2.Calculate_area();
         c2.display();
         circle c3 = new circle(12);
-        c3.Area();
+        c3.Calculate_area();
         c3.display();
     }
 }
